d036cf6b78d8c9f3dfc7751a3d98614fd81ea062
zekiyasin <zeki.kocyigit.2002@gmail.com>
2023-07-19 00:35:21 +0300

    carose

---
+++ src/components/carosel/Carosel.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "./carosel.css";
+import AppSwitcherIcon from "@atlaskit/icon/glyph/app-switcher";
+import IssuesIcon from "@atlaskit/icon/glyph/issues";
+import {BsGraphUpArrow} from "react-icons/bs";
+import {MdGTranslate} from "react-icons/md";
+import { Box } from "@atlaskit/primitives";
+import Button from "@atlaskit/button";
+const Carosel = () => {
+  const data = [
+    {
+      icon: <BsGraphUpArrow color="white" size="30" />,
+      title: "Sentiment Analiz Sorgu Hizmeti",
+      desc: "Our mission is to unleash the potential in every team.",
+    },
+    {
+      icon: <IssuesIcon primaryColor="white" size="large" label="" />,
+      title: "Bots Sorgu Hizmeti",
+      desc: "Our mission is to unleash the potential in every team.",
+    },
+    {
+      icon: <MdGTranslate color="white" size="30"  />,
+      title: "Translate Hizmeti",
+      desc: "Our mission is to unleash the potential in every team.",
+    },
+    {
+      icon: <AppSwitcherIcon primaryColor="white" size="large" label="" />,
+      title: "Aspect Base Sentiment",
+      desc: "Our mission is to unleash the potential in every team.",
+    },
+  ];
+
+  return (
+    <section className="carosel">
+      <div className="carosel-container">
+        <div className="carosel-text">
+          <h2>Portal Live Products</h2>
+          <p>
+            Basit sezgisel ve güzel deneyimler oluşturmak için Portal'ın
+            evrensel yazılım dilini kullanın.
+          </p>
+        </div>
+        <ul>
+          {data.map((item, index) => (
+            <li className="carosel-card" key={index}>
+              <div className="icon-box">
+                <Box>{item.icon}</Box>
+              </div>
+              <h3>{item.title}</h3>
+              <p>{item.desc}</p>
+              <Button>Default button</Button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Carosel;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import HeroSection from '../components/heroSection/HeroSection'
 import Products from '../components/products/Products'
+import Carosel from '../components/carosel/Carosel'
 const Home = () => {
   return (
     <div>
@@ -10,6 +11,11 @@ const Home = () => {
       <br/>
       <br/>
       <Products />
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <Carosel />
     </div>
   )
 }
